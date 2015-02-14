aa799bb676a3a7170c7e2c834a66b2f0bda87ac3
Wes <wesley.landriault@gmail.com>
2015-02-13 21:32:05 -0500

    Fixed reservation exception so that reservations that go into next day can still be accessed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,39 @@
 module ApplicationHelper
+
 	def reservation_time_options(restaurant)
-		times = (restaurant.opens_at...restaurant.closes_at).map do |hour|
-			[pretty_time(hour), hour]
+
+		def restaurant_times(start, finish)
+			(start..finish).map do |hour|
+			[pretty_time(hour), hour]	# options_for_select(times)
+			end
 		end
-		# options_for_select(times)
+
+		if resstaurant.opens_at == restaurant.closes_at
+			(0..23).map do |hour|
+			[pretty_time(hour), hour]
+		elsif restaurant.closes_at < restaurant.opens_at
+			times = restaurant_times(0, restaurant.closes_at).concat(restaurant_times(restaurant.opens_at, 23))	
+		else times = (restaurant.opens_at...restaurant.closes_at).map do |hour|
+			[pretty_time(hour), hour]
+			end
 		times
+		end
 	end
+
+
+		# if restaurant.closes_at <= restaurant.opens_at
+		# 	times1 = (0..restaurant.closes_at).map do |hour|
+		# 		[pretty_time(hour), hour]
+		# 	end
+
+		# 	times2 = (restaurant.opens_at..23).map do |hour|
+		# 		[pretty_time(hour), hour]
+		# 	end
+
+		# 	times1.concat(times2)
+		# 	times1
+			
+		
 
 	def restaurant_time_options
 		option_times = (0..23).map do |hour|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 def random_hour(from, to)
-  (Date.today + rand(from..to).hour + rand(0..60).minutes).to_datetime
+  rand(from..to)
 end
 
 puts "\nCreating users"
@@ -19,8 +19,8 @@ puts "\nCreating restaurants"
 	Restaurant.create!(
 		name: name,
 		capacity: [20, 40, 60, 200].sample,
-		opens_at: random_hour(9, 12),
-		closes_at: random_hour(16, 20),
+		opens_at: random_hour(0, 23),
+		closes_at: random_hour(0, 23),
 		description: Faker::Company.catch_phrase,
 		address: Faker::Address.street_address,
 		phone: Faker::PhoneNumber.phone_number
@@ -32,7 +32,8 @@ puts "\nCreating reservations"
 1000.times do
 	Reservation.create!(
 		party_size: [1,3,4,6,10].sample,
-		booking: random_hour(9, 18),
+		time: random_hour(9, 18),
+		date: Date.today,
 		user_id: User.all.sample.id,
 		restaurant_id: Restaurant.all.sample.id
 	)
